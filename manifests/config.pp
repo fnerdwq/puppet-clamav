a0a38bcc14ab3a6d6188999f31fb50713591e30c
Frederik Wagner <wagner@wagit.de>
2014-02-24 10:59:21 +0100

    added enhanced clamav user handling

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,12 +4,25 @@ class clamav::config {
   if $clamav::addgroup {
     $allow_supplementary_groups = true
 
-    user {'clamav':
-      groups => any2array($clamav::addgroup)
-    }
-
+    $user_additional_groups = any2array($clamav::addgroup)
   } else {
     $allow_supplementary_groups = false
+
+    $user_additional_groups = []
+  }
+
+  # neede to have cron update for unofficial sigs running
+  if $clamav::clamav_unofficial_sigs {
+    $user_shell = '/bin/bash'
+  } else {
+    $user_shell = '/bin/false'
+  }
+
+
+  user {'clamav':
+    groups => $user_additional_groups,
+    home   => $clamav::database_directory,
+    shell  => $user_shell,
   }
 
   file {'/etc/clamav/clamd.conf':
